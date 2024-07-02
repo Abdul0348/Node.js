ae4aae5cbb6ecc9eaa3acdc6b2b17d8dc227bfe7
Abdul0348 <arsalar286@gmail.com>
2024-07-03 00:48:16 +0500

    development environment fixed

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send({ data: 'Hello from server!' });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
